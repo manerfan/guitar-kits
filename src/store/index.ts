1e67d71f30fe0189d6843dc82066761cc3bda8b0
yongyong.fyy <yongyong.fyy@alibaba-inc.com>
2020-04-14 14:40:35 +0800

    切换到quasar框架

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import { store } from 'quasar/wrappers'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+// import example from './module-example'
+// import exampleState from './module-example/state'
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation
+ */
+
+export interface StoreInterface {
+  // Define your own store structure, using submodules if needed
+  // example: typeof exampleState;
+  example: unknown;
+}
+
+export default store(function ({ Vue }) {
+  Vue.use(Vuex)
+
+  const Store = new Vuex.Store({
+    modules: {
+      // example
+    },
+
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    strict: !!process.env.DEV
+  })
+
+  return Store
+})
